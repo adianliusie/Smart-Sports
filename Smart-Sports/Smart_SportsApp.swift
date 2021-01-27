18b55b5a0219ebc18ad69caa78a99b7be2a2794c
Adianliusie <adianliusie@gmail.com>
2021-01-27 15:35:32 +0400

    Initial Commit

---
+++ Smart-Sports/Smart_SportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_SportsApp.swift
+//  Smart-Sports
+//
+//  Created by Adian Liusie on 27/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Smart_SportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
